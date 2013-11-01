1ad82ec9c873f5498e0d20349fdbaecd8d74280a
hopestar720 <hopestar720@126.com>
2013-11-01 14:35:10 +0800

    add custom ui implement

---
+++ src/com/xhsoft/android/common/ui/AutoComplateTextViewWithClearIcon.java
@@ -0,0 +1,13 @@
+package com.xhsoft.android.common.ui;
+
+import android.content.Context;
+import android.widget.AutoCompleteTextView;
+
+public class AutoComplateTextViewWithClearIcon extends AutoCompleteTextView {
+
+	public AutoComplateTextViewWithClearIcon(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/xhsoft/android/common/ui/TextViewWithClearIcon.java
@@ -0,0 +1,13 @@
+package com.xhsoft.android.common.ui;
+
+import android.content.Context;
+import android.widget.EditText;
+
+public class TextViewWithClearIcon extends EditText {
+
+	public TextViewWithClearIcon(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
